3b2cf97f6b0c984ad3c585b65aa2676f67439a05
RickTran <67354999+vinhkhang97@users.noreply.github.com>
2026-06-29 14:30:42 +0700

    Create FUSA_Safety_Cadence.tcl

---
+++ FUSA_Safety_Cadence.tcl
@@ -0,0 +1,8 @@
+Functional Safety (FuSa) is a report that evaluates the safety of a system or design by analyzing potential faults and failures. 
+It categorizes these faults into specific types, such as Unobserved Undetected, Unobserved Detected, Dangerous Detected, and Dangerous Undetected. 
+The FuSa report includes metrics that help in assessing how safe the system is and serves as a tool for further analysis, such as FMEDA (Failure Modes, Effects, and Diagnostic Analysis) and ASIL (Automotive Safety Integrity Level) rating. 
+This report is crucial for ensuring that designs meet the required safety standards.
+There are 3 type in FUSA:
+  + DCLS: (Dual Core Clock Step): 
+  + TMR: (Triple Modular Redundancy):
+  + Bus protection: 
